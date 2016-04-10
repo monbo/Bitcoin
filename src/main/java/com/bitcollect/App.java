31ced09f9d972f98849a59a625e34ff84ca744cc
monbo <monbo@inbox.lt>
2016-04-10 14:33:35 +0900

    Newest April 2016 changes

--- src/main/java/com/bitcollect/App.java
+++ src/main/java/com/bitcollect/App.java
@@ -2,6 +2,12 @@ package com.bitcollect;
 //
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
+import com.xeiam.xchange.Exchange;
+import com.xeiam.xchange.ExchangeFactory;
+import com.xeiam.xchange.bitstamp.BitstampExchange;
+import com.xeiam.xchange.currency.CurrencyPair;
+import com.xeiam.xchange.dto.marketdata.Ticker;
+import com.xeiam.xchange.service.polling.marketdata.PollingMarketDataService;
 
 import java.io.IOException;
 import java.text.DateFormat;
@@ -17,106 +23,44 @@ import static com.bitcollect.ExchangeServers.setUpExhanges;
 
 
 class App {
-
-
-    //Names of bitcoin sites
-
-
     public static void main(String[] args) throws IOException, InterruptedException {
-        DateFormat df = new SimpleDateFormat("yy/MM/dd HH:mm:ss");
-
-        MongoClient client = new MongoClient("localhost", 27017);
-        MongoDatabase database = client.getDatabase("Tick");    // connect to database
-        System.out.println("Connect to database successfully");
-
-        //Setting up exchanges
-        setUpExhanges();
-        final int MYTHREADS = exchangeAmount;    // thread number
-        ExecutorService executor = Executors.newFixedThreadPool(MYTHREADS);
-        //noinspection InfiniteLoopStatement
 
         while (true) {
-            for (int i = 0; i < exchangeAmount; i++)
-            {
-                //System.out.println(bitcoinExchangekList[i].isInprogress());
-                if (!bitcoinExchangekList[i].isInprogress()) {
+            Exchange bitstamp = ExchangeFactory.INSTANCE.createExchange(BitstampExchange.class.getName());
 
-                    ThreadsList.BitcoinRunnable myRunnable = new ThreadsList.BitcoinRunnable(bitcoinExchangekList[i], database);
-                    executor.execute(myRunnable);
+            PollingMarketDataService marketDataService = bitstamp.getPollingMarketDataService();
 
+            Ticker ticker = marketDataService.getTicker(CurrencyPair.BTC_USD);
 
-                }
-                Thread.sleep(50);
+            System.out.println(ticker.toString());
+            try {
+                Thread.sleep(2000);                 //1000 milliseconds is one second.
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
             }
+            ;
         }
     }
-
-    //
-   /* public static class MyRunnable implements Runnable {
-        private final String url;
-        MongoCollection<Document> collection;
-
-        MyRunnable(String url, MongoDatabase database) {
-            this.url = url;
-            switch (url) {
-                case ExchangeServers.btceUSD:
-                    this.collection = database.getCollection("btceUSD");    // get database collection
-                    break;
-                case ExchangeServers.bitbayUSD:
-                    this.collection = database.getCollection("bitbayUSD");    // get database collection
-                    break;
-                case ExchangeServers.bitstampUSD:
-                    this.collection = database.getCollection("bitstampUSD");    // get database collection
-                    break;
-                case ExchangeServers.korbitKRW:
-                    this.collection = database.getCollection("korbitKRW");    // get database collection
-                    break;
-                case ExchangeServers.bitfinexUSD:
-                    this.collection = database.getCollection("bitfinexUSD");    // get database collection
-                    break;
-                case ExchangeServers.anxhkUSD:
-                    this.collection = database.getCollection("anxhkUSD");    // get database collection
-                    break;
-*//*					case ExchangeServers.bithumbKRW:
-						this.collection = database.getCollection("bithumbKRW");	// get database collection
-						break; // Kolkas neveikia del http request kaltes*//*
-                default:
-                    break;
-            }
-            //this.collection = database.getCollection("bitTestas");	// get database collection
-        }
-
-        @Override
-        public void run() {
-
-            try {
-
-
-                //if(ServerAccesabilityTimeCheck.ServerTimerCheck(String url)) {
-                Date now = new Date();
-                    *//*getTime():It returns the number of milliseconds since
-  				    * January 1, 1970, 00:00:00 GMT
-  				    * represented by this date.
-  				    *//*
-                Document doc = JsonReader.readJsonFromUrl(url);
-                doc.append("now", now.getTime()); // time at which the data was added
-
-                //}
-                //collection.insertOne(doc);
-                System.out.println(url + " " + doc.toString());
-            } catch (Exception e) {
-                System.err.println(e.getMessage());
-            }
-
-
-        }
-
-        private void FiveSecondDelay() {
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }*/
 }
+
+/*
+
+
+    DateFormat df = new SimpleDateFormat("yy/MM/dd HH:mm:ss");
+
+    MongoClient client = new MongoClient("localhost", 27017);
+    MongoDatabase database = client.getDatabase("Tick");    // connect to database
+    System.out.println("Connect to database successfully");
+    setUpExhanges();
+    final int MYTHREADS = exchangeAmount;    // thread number
+    ExecutorService executor = Executors.newFixedThreadPool(MYTHREADS);
+    for (int i = 0; i < exchangeAmount; i++) {
+        ThreadsList.BitcoinRunnable myRunnable = new ThreadsList.BitcoinRunnable(bitcoinExchangekList[i], database);
+        executor.execute(myRunnable);
+    }
+
+
+    while (true) {
+        Thread.sleep(10);
+    }
+}*/
--- src/main/java/com/bitcollect/ThreadsList.java
+++ src/main/java/com/bitcollect/ThreadsList.java
@@ -19,50 +19,45 @@ public class ThreadsList {
     private MongoCollection<Document> collection;
 
     public static class BitcoinRunnable implements Runnable {
-        public MongoDatabase database;
-        public BitcoinExchange bitcoinExchange;
-
+        private MongoDatabase database;
+        private  BitcoinExchange bitcoinExchange;
+        private MongoCollection<Document> collection;
         public BitcoinRunnable(BitcoinExchange bitcoinExchange, MongoDatabase database) {
-            MongoCollection<Document> collection;
             this.bitcoinExchange = bitcoinExchange;
             this.database = database;
+            collection = database.getCollection(ThreadListCollections.chooseCollection(bitcoinExchange));
         }
 
         @Override
         public void run() {
-            try {
-                bitcoinExchange.setInprogress(true);
-                MongoCollection<Document> collection = null;
-
-                collection = database.getCollection(ThreadListCollections.chooseCollection(bitcoinExchange));
-
-                Document doc = JsonReader.readJsonFromUrl(bitcoinExchange);
-                //System.out.println("FAKE  " + bitcoinExchange.getServeURL() + " " + doc.toString());
-                if (doc != null) {
+            while(true) {
+                try {
+                    Document doc = JsonReader.readJsonFromUrl(bitcoinExchange);
+                    //System.out.println("FAKE  " + bitcoinExchange.getServeURL() + " " + doc.toString());
+                    if (doc != null) {
                         if (!StringDuplicate.check(doc.toString()).equals(bitcoinExchange.getPrevious())) {
                             bitcoinExchange.setPrevious(StringDuplicate.check(doc.toString()));
                             Date now = new Date();
                             doc.append("now", now.getTime()); // time at which the data was added
                             if (collection != null) {
                                 collection.insertOne(doc);
-                                //System.out.println(bitcoinExchange.getServeURL() + " " + doc.toString());
+                                System.out.println(bitcoinExchange.getServeURL() + " " + doc.toString());
                             }
                         }
-                }
+                    }
 /*              DateFormat df = new SimpleDateFormat("yy/MM/dd HH:mm:ss");
                 if(bitcoinExchange.getServeURL() == ExchangeServers.okcoinCNY) {
                     Date dates = new Date();
                     System.out.println(df.format(dates) + "   "+ bitcoinExchange.getDelay());
                 }*/
-                //System.out.println();
-                Delay(bitcoinExchange.getDelay());
+                    //System.out.println();
+                    Delay(bitcoinExchange.getDelay());
 
-                bitcoinExchange.setInprogress(false);
-
-            } catch (Exception e) {
-                System.err.println(e.getMessage());
+                    //bitcoinExchange.setInprogress(false);
+                } catch (Exception e) {
+                    System.err.println(e.getMessage());
+                }
             }
-
         }
         private void Delay(int delay) {
             try {
